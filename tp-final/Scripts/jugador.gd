c8631137f87820d3ff9a7740786e9874ac228918
Lautaro Eugster <Lautaroeugster@hotmail.com.ar>
2026-02-04 02:55:09 -0300

    Movimiento Nave
    Movimiento simple y rápido de la nave usando W, A, S, D o las flechas.

--- tp-final/Scripts/jugador.gd
+++ tp-final/Scripts/jugador.gd
@@ -1,27 +1,16 @@
 extends CharacterBody2D
 
-@export var velocidadMax := 200.0
-@export var aceleracion := 1200.0
-@export var friccion := 1000.0
+@export var velocidad := 500.0
 
-func _physics_process(delta):
-	var direccion := Vector2.ZERO
+func _physics_process(_delta):
+	var dir := Vector2.ZERO
 	
-	direccion.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	direccion.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	if direccion != Vector2.ZERO:
-		direccion = direccion.normalized()
+	if dir != Vector2.ZERO:
+		dir = dir.normalized()
 		
-		# Velocidad objetivo.
-		var velObjetivo = direccion * velocidadMax
-		
-		# Acelerar suavemente hacia esa velocidad.
-		velocity = velocity.move_toward(velObjetivo, aceleracion * delta)
-	else:
-		#Si no hay input, frenar suavemente.
-		velocity = velocity.move_toward(Vector2.ZERO, friccion * delta)
-	
-	
+	velocity = dir * velocidad
 	
 	move_and_slide()
