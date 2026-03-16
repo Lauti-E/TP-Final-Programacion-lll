1dd4bdc815cad8b313fd3e0c9a803c600d454379
Lautaro Eugster <Lautaroeugster@hotmail.com.ar>
2026-03-16 19:43:25 -0300

    Jugador
    Cambio en el script del Jugador.

--- tp-final/Scripts/jugador.gd
+++ tp-final/Scripts/jugador.gd
@@ -1,49 +1,30 @@
 extends CharacterBody2D
 
-# Varibles de la nave.
+# Varibles del personaje.
 @export var velocidad := 500.0
 
 # Variables del proyectil.
 @export var escenaProyectil: PackedScene
-@export var cadenciaDisparo: float = 0.5
+@export var cadenciaDisparo: float = 0.2
 
+# Variables de disparo.
 var puedeDisparar: bool = true
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("disparar") and puedeDisparar:
 		Disparar()
 
 func _ready():
 	$Personaje.play("idle")
 
-func Disparar():
-	puedeDisparar = false
-	
-	var proyectil = escenaProyectil.instantiate()
-	
-	proyectil.global_position = $"Punto Proyectil Der".global_position
-	
-	get_tree().current_scene.add_child(proyectil)
-	
-	await get_tree().create_timer(cadenciaDisparo).timeout
-	
-	puedeDisparar = true
-
 func _physics_process(_delta):
-	# Movimiento de la nave.
-	var dir := Vector2.ZERO
-	
-	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
-	if dir != Vector2.ZERO:
-		dir = dir.normalized()
+	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 		
 	velocity = dir * velocidad
 	
 	move_and_slide()
 	
-	# Limitar el movimiento de la nave dentro de la cámara.
+	# Limitar el movimiento del personaje dentro de la cámara.
 	var camara = $Camera2D
 	
 	var izq = camara.limit_left
@@ -57,3 +38,16 @@ func _physics_process(_delta):
 	
 	global_position.x = clamp(global_position.x, izq + extents.x + 25, der - extents.x - 25)
 	global_position.y = clamp(global_position.y, arriba + extents.y - 25, abajo - extents.y - 25)
+
+func Disparar():
+	puedeDisparar = false
+	
+	var proyectil = escenaProyectil.instantiate()
+	
+	proyectil.global_position = $"Punto Proyectil Der".global_position
+	
+	get_tree().current_scene.add_child(proyectil)
+	
+	await get_tree().create_timer(cadenciaDisparo).timeout
+	
+	puedeDisparar = true
