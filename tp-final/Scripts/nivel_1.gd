0cecc3fa53a7d8f1660c48c5e4ddcf513c0145b5
Lautaro Eugster <Lautaroeugster@hotmail.com.ar>
2026-04-22 04:38:37 -0300

    Parallax
    Nuevo Parallax.

--- tp-final/Scripts/nivel_1.gd
+++ tp-final/Scripts/nivel_1.gd
@@ -1,1 +1,10 @@
 extends Node
+
+@onready var espacioLayer: ParallaxLayer = %EspacioLayer
+@onready var estrellasLejanasLayer: ParallaxLayer = %EstrellasLejanasLayer
+@onready var estrellasCercanasLayer: ParallaxLayer = %EstrellasCercanasLayer
+
+func _process(delta):
+	espacioLayer.motion_offset.y += 2 * delta
+	estrellasLejanasLayer.motion_offset.y += 5 * delta
+	estrellasCercanasLayer.motion_offset.y += 20 * delta
